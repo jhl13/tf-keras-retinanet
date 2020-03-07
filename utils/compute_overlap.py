fbcbc9816ecb14e6b5daeee1bb2fcd67312280df
luo <1249954696@qq.com>
2020-03-07 22:08:06 +0800

    [eval] 获取检测结果

--- utils/compute_overlap.py
+++ utils/compute_overlap.py
@@ -1,9 +1,6 @@
 # -*- coding:utf-8 -*-
 import numpy as np
 
-boxes = np.asarray([[0, 0, 10, 10], [0, 0, 5, 5]])
-gt = np.asarray([[0, 0, 6, 6], [2, 2, 6, 6]])
-
 def compute_overlap(boxes, gt):
     """
     Args
@@ -16,6 +13,7 @@ def compute_overlap(boxes, gt):
     boxes = np.asarray(boxes)
     gt = np.asarray(gt)
     
+    # 边缘像素也要算进面积
     area_gt = (gt[:, 2] - gt[:, 0] + 1) * (gt[:, 3] - gt[:, 1] + 1)
 
     w_overlap = np.minimum(np.expand_dims(boxes[:, 2], axis=1), gt[:, 2]) \
@@ -34,5 +32,3 @@ def compute_overlap(boxes, gt):
     intersection = w_overlap * h_overlap
 
     return intersection / ua
-
-compute_overlap(boxes, gt)
--- utils/map_eval.py
+++ utils/map_eval.py
@@ -1,7 +1,10 @@
 # -*- coding:utf-8 -*-
-from utils.compute_overlap import compute_overlap
+from .compute_overlap import compute_overlap
 import numpy as np
-
+from tqdm import tqdm
+import time
+import cv2
+import os
 
 def _compute_ap(recall, precision):
     """ Compute the average precision, given the recall and precision curves.
@@ -32,3 +35,70 @@ def _compute_ap(recall, precision):
     # and sum (\Delta recall) * prec # 计算PR曲线的面积
     ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
     return ap
+
+
+def _get_detections(generator, model, score_threshold=0.05, max_detections=100, save_path=None):
+    """ Get the detections from the model using the generator.
+
+    The result is a list of lists such that the size is:
+        all_detections[num_images][num_classes] = detections[num_detections, 4 + scores]
+
+    # Arguments
+        generator       : The generator used to run images through the model.
+        model           : The model to run on the images.
+        score_threshold : The score confidence threshold to use.
+        max_detections  : The maximum number of detections to use per image.
+        save_path       : The path to save the images with visualized detections to.
+    # Returns
+        A list of lists containing the detections for each image in the generator.
+    """
+    all_detections = [[None for i in range(generator.num_classes())] for j in range(generator.size())]
+    all_inferences_time = [None for i in range(generator.size())]
+    pbar = tqdm(total = generator.size())
+
+    # 只使用"channel last"
+    for i in range(generator.size()):
+        raw_image    = generator.load_image(i)
+        image        = generator.preprocess_image(raw_image.copy())
+        # 返回resize的尺寸 resized_image.shape / original_image.shape
+        image, scale = generator.resize_image(image)
+
+        # run network
+        # boxes scores labels 一一对应
+        start = time.time()
+        boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))[:3]
+        inference_time = time.time() - start
+
+        # 把boxes还原成原来的图片的大小
+        boxes /= scale
+
+        # 大于阈值分数索引
+        indices = np.where(scores[0, :] > score_threshold)[0]
+
+        # 选取对应索引的分数
+        scores = scores[0][indices]
+
+        # argsort 是从小到大排序 
+        scores_sort = np.argsort(-scores)[:max_detections]
+
+        # 选取对应索引且重新排序的输出 （对得到的索引排序就可以了）
+        image_boxes      = boxes[0, indices[scores_sort], :]
+        image_scores     = scores[scores_sort]
+        image_labels     = labels[0, indices[scores_sort]]
+        image_detections = np.concatenate([image_boxes, np.expand_dims(image_scores, axis=1), np.expand_dims(image_labels, axis=1)], axis=1)
+        
+        # 可视化检测结果
+        if save_path is not None:
+            draw_annotations(raw_image, generator.load_annotations(i), label_to_name=generator.label_to_name)
+            draw_detections(raw_image, image_boxes, image_scores, image_labels, label_to_name=generator.label_to_name, score_threshold=score_threshold)
+            cv2.imwrite(os.path.join(save_path, '{}.png'.format(i)), raw_image)
+
+        for label in range(generator.num_classes()):
+            if not generator.has_label(label):
+                continue
+            # 将检测框拷贝到相应的位置
+            all_detections[i][label] = image_detections[image_detections[:, -1] == label, :-1]
+
+        all_inferences_time[i] = inference_time
+    
+    return all_detections, all_inferences_time
